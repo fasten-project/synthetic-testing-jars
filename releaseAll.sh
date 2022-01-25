8a81782682b1ceafe6bf844975d9770e0003928b
ashkboos <m.keshani@tudelft.nl>
2022-01-25 19:13:26 +0100

    modify release to also include source jars

--- releaseAll.sh
+++ releaseAll.sh
@@ -2,6 +2,24 @@
 
 mvn clean package
 
-./release.sh pom.xml pom.xml
-./release.sh app/pom.xml app/target/app-0.0.1.jar
-./release.sh lib/pom.xml lib/target/lib-0.0.1.jar
+LOCAL_REPO=release/
+
+mvn install:install-file \
+	-DpomFile=pom.xml \
+	-Dfile=pom.xml \
+	-DlocalRepositoryPath=$LOCAL_REPO \
+	-DcreateChecksum=true
+
+BASE="app/target/app-0.0.1"
+mvn install:install-file \
+	-DpomFile=app/pom.xml \
+	-Dfile=$BASE.jar \
+	-Dsources=$BASE-sources.jar \
+	-DlocalRepositoryPath=$LOCAL_REPO \
+	-DcreateChecksum=true
+
+mvn install:install-file \
+	-DpomFile=lib/pom.xml \
+	-Dfile=lib/target/lib-0.0.1.jar \
+	-DlocalRepositoryPath=$LOCAL_REPO \
+	-DcreateChecksum=true
